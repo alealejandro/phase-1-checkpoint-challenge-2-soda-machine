2240a2bfcf949ece6a60ecd21dbc2d245c5834e3
Alex <alex.m.4155@gmail.com>
2016-11-05 15:02:26 -0700

    Create instance methods for soda machine class

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.find do |soda|
+      soda.brand == soda_brand
+    end
   end
 
   def sell(soda_brand)
+    found_soda = find_soda(soda_brand)
+    if found_soda
+      @cash += found_soda.price
+      sodas.delete(found_soda)
+    end
+    found_soda
   end
 
 end
